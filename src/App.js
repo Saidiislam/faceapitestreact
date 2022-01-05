18b5c1e25ba56bea4367e56ee09559d954e6fb9d
Saidiislam <saidisinnerd@gmail.com>
2022-01-05 23:58:58 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      {/* <video id="videoInput" style={{width:'720px', height:'550px'}} muted autoPlay></video> */}
+    </div>
+  );
+}
+
+export default App;
